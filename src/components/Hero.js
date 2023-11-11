8467ebf84f90ee2a158d29e34a6d77ff7b83bb37
Mikey Lee <124496222+relaxmikeydoesit@users.noreply.github.com>
2023-11-11 14:03:18 -0500

    updated commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,12 +2,12 @@ import React from "react";
 import styles from '@/styles/Hero.module.css';
 
 export default function Hero(props) {
-    console.log(props);
+    //console.log(props);
 
   return (
     <> 
     <div className={styles.hero} style={{backgroundImage:`url(${props.heroImage})`}}>
-      {props.header}
+     
     </div>
     </>
     )   
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,31 @@
 import React from "react";
 import styles from '@/styles/Home.module.css';
 import Hero from '@/components/Hero';
+import Navbar from '@/components/Navbar';
+import Link from 'next/link';
+import Card from '@/components/Card';
+
+
 
 export default function Home() {
   return (
   <>
   <Hero heroImage='/hero.jpg' height='400px' header='main header' />
-  <h1 className={styles.bigRed}>HOME</h1>
-
-  <p>This is the landing page for our Next.js app.</p>
-  <p className={styles.blueFont}>It is a static page that is rendered at build time, and this paragraph should be blue.</p>
+  <Navbar className={styles.navbar} style={{}}>
+  <Link href='/'>Home ||</Link>
+  <Link href='/'>About || </Link>
+  <Link href='/'>Contact ||</Link>
+  <Link href='/'>Need Help?</Link>
+  </Navbar>
   
-  </>  
+  <div className={styles.cards}>
+  <h1 className={styles.yellowFont}>HOME</h1>
+  <p className={styles.whiteFont}>This is the landing page for our Next.js app. It is a static page that is rendered at build time, and this paragraph should be white and italic.</p> 
+  </div>
+ <Card className={styles.card} style={{}}>
+  <h1 className={styles.yellowFont}>ABOUT</h1>
+  </Card>
+ 
+</>
   )
 }
